5c45e61df32c36acad0ae066c00bd0048c0fe35c
amanj adnan <amanjadnan7@gmail.com>
2021-07-07 00:50:23 +0300

    add test for model and controllers

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context  "validation test" do
+
+    it 'ensure name presence' do
+      post= Post.new(title:"human" , content:'lols').save
+
+      expect(post).to eq(false )
+    end
+
+    it 'Should save successfully' do
+      post= Post.new(name:"amanj" ,  title:"human" , content:'lols').save
+
+      expect(post).to eq(true )
+    end
+
+  end
+
+  context "scope test" do
+
+  end
+
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -21,32 +21,41 @@ RSpec.describe "/users", type: :request do
 
 
   describe "GET /index" do
-  it "renders a successful response" do
-    Post.create!
-    get posts_url
-    expect(response).to be_successful
+    it "renders a successful response" do
+
+      get posts_url
+      expect(response).to be_successful
+    end
   end
-end
 
   describe "GET /show" do
-  it "renders a successful response" do
-    post = Post.create!
-    get post_url(post)
-    expect(response).to be_successful
+    it "renders a successful response" do
+      post = Post.create!(name:"amanj" ,  title:"human" , content:'lols')
+      get post_url(post)
+      expect(response).to be_successful
+    end
   end
-end
 
   describe "GET /new" do
-  it "renders a successful response" do
-    get new_post_url
-    expect(response).to be_successful
+    it "renders a successful response" do
+      get new_post_url
+      expect(response).to be_successful
+    end
   end
-end
+
+  describe "GET /edit" do
+    it "render a successful response" do
+      post = Post.create!(name:"amanj" ,  title:"human" , content:'lols')
+      get edit_post_url(post)
+      expect(response).to be_successful
+    end
+  end
+
 
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new User" do
-        # post = Post.first_or_create!(name:"any thing" , title:"any thing" , content:"any thing" )
+
         expect {
           post posts_url, params: valid_params
         }.to change(Post, :count).by(1)
@@ -59,9 +68,40 @@ end
       end
     end
 
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+         {
+          name:"ahmad",
+          title:"lols",
+          content:"ols"
+        }
+      }
+
+      it "updates the requested user" do
+        post = Post.create!(name:"amanj" ,  title:"human" , content:'lols')
+        patch post_url(post), params: { post: new_attributes }
+        post.reload
+        expect(post.name).to eq "ahmad"
+        expect(response).to redirect_to(post_url(post))
+      end
+
+    end
+
     end
 
 
+  describe "DELETE /destroy" do
+    it "destroys the requested user" do
+      post = Post.create!(name:"amanj" ,  title:"human" , content:'lols')
+      expect {
+        delete post_url(post)
+      }.to change(Post, :count).by(-1)
+    end
 
+
+  end
 
 end
